0d9932fd39d3703f4b9a652b98bae75e06bad790
Anton Voronin <hestheone30@gmail.com>
2020-04-01 13:58:18 +0300

    add more test cases for String and StringUtil classes

--- UnitTests/TestString.cs
+++ UnitTests/TestString.cs
@@ -8,6 +8,9 @@ namespace UnitTests
     [Test]
     public void TestToString()
     {
+      /// <summary>
+      /// Create different String objects, check that they al are returned as strings correctly
+      /// </summary>
       String a = new String();
       String b = new String('b');
       String c = new String("Hello");
@@ -21,6 +24,9 @@ namespace UnitTests
     [Test]
     public void TestClear()
     {
+      /// <summary>
+      /// Create several String objects, call Clear() method, check that all of them are empty
+      /// </summary>
       String a = new String();
       String b = new String('c');
       String c = new String("TEST");
@@ -33,6 +39,32 @@ namespace UnitTests
       Assert.AreEqual("", b.ToString());
       Assert.AreEqual("", c.ToString());
       Assert.AreEqual("", d.ToString());
+    }
+
+    [Test]
+    public void TestIStringPresenterImplementation()
+    {
+      /// <summary>
+      /// Create string util object, check that it returns correct char array
+      /// via ValueAsCharArray() method
+      /// </summary>
+      String a = new String("Test");
+      char[] contents = a.ValueAsCharArray();
+      Assert.AreEqual('T', contents[0]);
+      Assert.AreEqual('e', contents[1]);
+      Assert.AreEqual('s', contents[2]);
+      Assert.AreEqual('t', contents[3]);
+
+      String b = new String();
+      contents = b.ValueAsCharArray();
+      Assert.AreEqual(0, contents.Length);
+
+      String d = new String(new char[] { 'T', 'e', 's', 't' });
+      contents = d.ValueAsCharArray();
+      Assert.AreEqual('T', contents[0]);
+      Assert.AreEqual('e', contents[1]);
+      Assert.AreEqual('s', contents[2]);
+      Assert.AreEqual('t', contents[3]);
     }
   }
 }
--- UnitTests/TestStringUtil.cs
+++ UnitTests/TestStringUtil.cs
@@ -11,22 +11,57 @@ namespace UnitTests
     [Test]
     public void TestSubStringOneParam()
     {
+      /// <summary>
+      /// Create StringUtil object, call substring method with one param
+      /// check that it works correctly
+      /// Check that method throws expection in case of incorrect arguments
+      /// </summary>
       StringUtil util = new StringUtil(new String("Test"));
       Assert.AreEqual("est", util.SubString(1).ToString());
       Assert.AreEqual("", util.SubString(4).ToString());
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(-1));
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(5));
+      Assert.AreEqual("Test", util.ToString());
     }
 
     [Test]
     public void TestSubStringTwoParams()
     {
+      /// <summary>
+      /// Create StringUtil object, call substring method with two params
+      /// check that it works correctly
+      /// Check that method throws expection in case of incorrect arguments
+      /// </summary>
       StringUtil util = new StringUtil(new String("Test"));
       Assert.AreEqual("es", util.SubString(1, 2).ToString());
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(-1, 3));
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(5, 3));
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(1, 6));
       Assert.Throws<ArgumentOutOfRangeException>(() => util.SubString(1, -1));
+      Assert.AreEqual("Test", util.ToString());
+    }
+
+    [Test]
+    public void TestIStringPresenterImplementation()
+    {
+      /// <summary>
+      /// Create string util object, check that it returns correct char array
+      /// via ValueAsCharArray() method
+      /// </summary>
+      StringUtil util = new StringUtil(new String("Test"));
+      char[] contents = util.ValueAsCharArray();
+      Assert.AreEqual('T', contents[0]);
+      Assert.AreEqual('e', contents[1]);
+      Assert.AreEqual('s', contents[2]);
+      Assert.AreEqual('t', contents[3]);
+
+      //Call SubString() method, check that contents remained the same
+      util.SubString(1);
+      contents = util.ValueAsCharArray();
+      Assert.AreEqual('T', contents[0]);
+      Assert.AreEqual('e', contents[1]);
+      Assert.AreEqual('s', contents[2]);
+      Assert.AreEqual('t', contents[3]);
     }
   }
 }
