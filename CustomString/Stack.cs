df17b267bf6b2d1d4efb64a99370ceebd5bb3c03
Anton Voronin <hestheone30@gmail.com>
2020-04-01 13:58:42 +0300

    add Stack class and unit tests for it

---
+++ CustomString/Stack.cs
@@ -0,0 +1,90 @@
+﻿using CustomString.Abstract;
+using System;
+
+namespace CustomString
+{
+  public class Stack<T> where T : IStringPresenter
+  {
+    public int Size { get; private set; }
+    private const int DEFAULT_CAPACITY = 16;
+    private const float EXPAND_PART = 0.5f;
+    private const int EXPAND_MULTIPLIER = 2;
+    private T[] elements;
+
+    public Stack()
+    {
+      elements = new T[DEFAULT_CAPACITY];
+      Size = 0;
+    }
+
+    public Stack(int initialCapacity)
+    {
+      elements = new T[initialCapacity];
+      Size = 0;
+    }
+
+    public T Pop()
+    {
+      if (Size == 0)
+      {
+        throw new InvalidOperationException("Stack is empty");
+      }
+      else
+      {
+        var value = elements[Size - 1];
+        elements[Size - 1] = default;
+        Size--;
+        return value;
+      }
+    }
+
+    public void Push(T element)
+    {
+      if (Size > elements.Length * EXPAND_PART)
+      {
+        T[] expandedStack = new T[elements.Length * EXPAND_MULTIPLIER];
+        elements.CopyTo(expandedStack, 0);
+        elements = expandedStack;
+      }
+      elements[Size] = element;
+      Size++;
+    }
+
+    public T Peek()
+    {
+      if (Size == 0)
+      {
+        throw new InvalidOperationException("Stack is empty");
+      }
+      else
+      {
+        return elements[Size - 1];
+      }
+    }
+
+    public T this[int i]
+    {
+      get
+      {
+        if (i >= Size)
+        {
+          throw new IndexOutOfRangeException();
+        }
+        return elements[i];
+      }
+    }
+
+    public int isEmpty()
+    {
+      return Size == 0 ? 1 : 0;
+    }
+
+    public void ViewContents()
+    {
+      for (int i = Size - 1; i >= 0; i--)
+      {
+        Console.WriteLine(elements[i].ToString());
+      }
+    }
+  }
+}
---
+++ UnitTests/TestStack.cs
@@ -0,0 +1,123 @@
+﻿using CustomString;
+using CustomString.Abstract;
+using NUnit.Framework;
+using System;
+using String = CustomString.String;
+
+namespace UnitTests
+{
+  public class TestStack
+  {
+    [Test]
+    public void TestPop()
+    {
+      /// <summary>
+      /// Create stack, add oject, call Pop() method, check that it returned correct result
+      /// call Pop() again, expect to get Exception
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      stack.Push(new String("Test"));
+      Assert.AreEqual("Test", stack.Pop().ToString());
+      Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
+    [Test]
+    public void TestPopCorrectOrder()
+    {
+      /// <summary>
+      /// Check that Pop method actually returns objects First In First Out
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      stack.Push(new String("Test"));
+      stack.Push(new String("Test2"));
+      stack.Push(new String("Test3"));
+      Assert.AreEqual("Test3", stack.Pop().ToString());
+      Assert.AreEqual("Test2", stack.Pop().ToString());
+      Assert.AreEqual("Test", stack.Pop().ToString());
+      Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
+    [Test]
+    public void TestPush()
+    {
+      /// <summary>
+      /// Create stack, add oject, check stack size
+      /// add one more object, chack stack size
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      stack.Push(new String("Test"));
+      Assert.AreEqual(1, stack.Size);
+      stack.Push(new String("Test2"));
+      Assert.AreEqual(2, stack.Size);
+    }
+
+    [Test]
+    public void TestStackExpand()
+    {
+      /// <summary>
+      /// Create stack with size 1, add object, check stack size
+      /// add one more object, check stack size, expect no errors
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>(1);
+      stack.Push(new String("Test"));
+      Assert.AreEqual(1, stack.Size);
+      Assert.AreEqual("Test", stack.Peek().ToString());
+      stack.Push(new String("Test2"));
+      Assert.AreEqual(2, stack.Size);
+      Assert.AreEqual("Test2", stack.Peek().ToString());
+    }
+
+    [Test]
+    public void TestFIFOLogic()
+    {
+      /// <summary>
+      /// Create stack, add and delete objects in different order
+      /// check that stack works as expected
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      stack.Push(new String("Test"));
+      stack.Push(new String("Test2"));
+      stack.Push(new String("Test3"));
+      Assert.AreEqual(3, stack.Size);
+      Assert.AreEqual("Test3", stack.Pop().ToString());
+      Assert.AreEqual("Test2", stack.Pop().ToString());
+      stack.Push(new String("Test4"));
+      stack.Push(new String("Test5"));
+      Assert.AreEqual("Test5", stack.Pop().ToString());
+      Assert.AreEqual("Test4", stack.Pop().ToString());
+      Assert.AreEqual("Test", stack.Pop().ToString());
+    }
+
+    [Test]
+    public void TestPeek()
+    {
+      /// <summary>
+      /// Create stack, add object to stack, call Peek()
+      /// check that stack size remained the same
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      stack.Push(new String("Test"));
+      Assert.AreEqual(1, stack.Size);
+      Assert.AreEqual("Test", stack.Peek().ToString());
+      Assert.AreEqual(1, stack.Size);
+    }
+
+    [Test]
+    public void TestIsEmpty()
+    {
+      /// <summary>
+      /// Create stack, check that it's empty
+      /// add object to stack, check that stack's not empty
+      /// remove object from stack, check that stack is empty
+      /// </summary>
+      Stack<IStringPresenter> stack = new Stack<IStringPresenter>();
+      Assert.AreEqual(1, stack.isEmpty());
+      stack.Push(new String("Test"));
+      Assert.AreEqual(0, stack.isEmpty());
+      stack.Peek();
+      Assert.AreEqual(0, stack.isEmpty());
+      stack.Pop();
+      Assert.AreEqual(1, stack.isEmpty());
+    }
+  }
+}
